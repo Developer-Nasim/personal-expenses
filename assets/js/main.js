e4825cc89af9884e6bb5db84e5192b932e6ddb7c
AJ Nasim <61669109+Developer-Nasim@users.noreply.github.com>
2021-04-03 02:34:22 +0600

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,9 @@
- 
+ "use strict";
+
+let drpMenu = document.querySelector('.table-bottom')
+let Menus   = document.querySelector('.drpmenu')
+
+drpMenu.addEventListener('click', () => {
+    drpMenu.classList.toggle('active')
+    Menus.classList.toggle('active')
+})
